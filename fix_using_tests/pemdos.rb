27b48bedadc8147bb32733985fc162d4fcd86f64
Kendall Stephens <kendoll.stephens@gmail.com>
2020-06-28 23:20:11 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,10 @@
-# don't forget to add: require 'pry'
+require 'pry'
 
 def snake_it_up(string)
+  number = 10
   if string[0] == "s"
-    10 * "s" + string
-  else
-    string
+    "s" * number + string
+  elsif
+    string 
   end
 end
